f585ea6663b633f67bd858a26f3c43aa9c4d23fa
Gavin-Toole <121550462+Gavin-Toole@users.noreply.github.com>
2023-03-09 16:22:03 -0330

    initial  code to read csv files

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,1 +1,13 @@
 # This is the code for PyBank
+#Import 
+import os
+import csv
+
+# Path to collect data
+bank_CSV = os.path.join("Resources", "budget_data.csv")
+with open(bank_CSV, 'r') as f:
+    reader =csv.reader(f,delimiter=",")
+    header_row = next(f)
+
+    for row in reader:
+     
--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,1 +1,15 @@
 # This is the code for PyPoll
+# import dependances
+import os
+import csv
+
+# Path to read csv
+poll_csv = os.path.join("Resources", "election_data.csv")
+
+# Read csv
+with open(poll_csv, 'r') as f:
+    reader = csv.reader(f, delimiter=",")
+    header_row = next(f)
+    
+    for row in reader:
+        print(row)
